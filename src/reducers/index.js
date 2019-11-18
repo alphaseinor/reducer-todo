1730027ecf2abd694615e86da7a65409aaa95182
Brian Hague <alphaseinor@gmail.com>
2019-11-18 01:17:33 -0600

    updated add case to include uuid as id and completed fields

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
+import {uuidv1 as uuid} from 'uuid/v1'
+
 export const initialState = {
     item: 'Learn about reducers',
     completed: false,
-    id: 3892987589
+    id: uuid
 }
 
 export const todoReducer = (state, action) => {
   switch (action.type){
     case "ADD_ITEM":
-      return {...state, item: action.payload}
+      return {...state, item: action.payload, completed: false, id: uuid}
     case "TOGGLE_DONE":
       return {...state, completed: !state.completed}
     default:
